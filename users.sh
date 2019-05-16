c33d445eb4166fb508a41257d1b408c043a6d4f9
juliaaano <juliaaano@gmail.com>
2019-05-16 15:24:37 +1000

    Add git hook stub implementation

--- users.sh
+++ users.sh
@@ -3,13 +3,13 @@
 set -euxo pipefail
 
 JBOSS_HOME=./target/jboss-eap-7.2
-ADMIN_USER=${1:-adminUser}
+ADMIN_USR=${1:-adminUser}
 ADMIN_PWD=${2:-password}
-CONTROLLER_USER=${3:-controllerUser}
+CONTROLLER_USR=${3:-controllerUser}
 CONTROLLER_PWD=${4:-password}
 
-$JBOSS_HOME/bin/add-user.sh -a --user $ADMIN_USER --password $ADMIN_PWD --role admin
-$JBOSS_HOME/bin/add-user.sh -a --user $CONTROLLER_USER --password $CONTROLLER_PWD --role rest-all
+$JBOSS_HOME/bin/add-user.sh -a --user $ADMIN_USR --password $ADMIN_PWD --role admin
+$JBOSS_HOME/bin/add-user.sh -a --user $CONTROLLER_USR --password $CONTROLLER_PWD --role rest-all
 
 echo "end of script"
 
