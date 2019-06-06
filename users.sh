3abe65c7141181ae07520b6092cb460b90bea649
juliaaano <juliaaano@gmail.com>
2019-06-07 09:37:32 +1000

    Add more repos and mirrors to settings.xml

--- users.sh
+++ users.sh
@@ -3,10 +3,10 @@
 set -euxo pipefail
 
 JBOSS_HOME=./target/jboss-eap-7.2
-KIESERVER_USER=${1:-kieserverUser}
+KIESERVER_USR=${1:-kieserverUser}
 KIESERVER_PWD=${2:-password}
 
-$JBOSS_HOME/bin/add-user.sh -a --user $KIESERVER_USER --password $KIESERVER_PWD --role kie-server
+$JBOSS_HOME/bin/add-user.sh -a --user $KIESERVER_USR --password $KIESERVER_PWD --role kie-server
 
 echo "end of script"
 
