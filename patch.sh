d321c880477a5874fcefe56eccf29e4169b4fff6
juliaaano <juliaaano@gmail.com>
2019-07-24 10:52:53 +1000

    Upgrade to RHDM 7.4.0 with EAP 7.2 patches

--- patch.sh
+++ patch.sh
@@ -2,9 +2,10 @@
 
 set -euxo pipefail
 
-PATCH_DIR=/home/ec2-user/rhdm-central/downloads
+PATCH_DIR=$(pwd)/downloads
 PATCH_URL=https://s3-ap-southeast-2.amazonaws.com/public.juliaaano
 JBOSS_PATCH01_ZIP=jboss-eap-7.2.1-patch.zip
+JBOSS_PATCH02_ZIP=jboss-eap-7.2.2-patch.zip
 JBOSS_DIR=./target/jboss-eap-7.2
 
 MODE=${1:-online}
@@ -13,9 +14,11 @@ mkdir -p $PATCH_DIR
 
 if [ $MODE != "offline" ]; then
     curl -o $PATCH_DIR/$JBOSS_PATCH01_ZIP $PATCH_URL/$JBOSS_PATCH01_ZIP
+    curl -o $PATCH_DIR/$JBOSS_PATCH02_ZIP $PATCH_URL/$JBOSS_PATCH02_ZIP
 fi
 
-$JBOSS_DIR/bin/jboss-cli.sh --command="patch apply $PATCH_DIR/jboss-eap-7.2.1-patch.zip"
+$JBOSS_DIR/bin/jboss-cli.sh --command="patch apply $PATCH_DIR/$JBOSS_PATCH01_ZIP"
+$JBOSS_DIR/bin/jboss-cli.sh --command="patch apply $PATCH_DIR/$JBOSS_PATCH02_ZIP"
 
 echo "end of script"
 
