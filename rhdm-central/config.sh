606e4f331a4b8806bab0505c8b785579352ea6c6
Juliano Mohr <juliaaano@gmail.com>
2019-11-29 11:39:35 +1100

    Upgrade RHDM to 7.5.0

--- rhdm-central/config.sh
+++ rhdm-central/config.sh
@@ -3,7 +3,7 @@
 DOWNLOADS_URL=https://s3-ap-southeast-2.amazonaws.com/public.juliaaano
 DOWNLOADS_DIR=./downloads
 INSTALLATION_DIR=./target
-RHDM_CENTRAL_ZIP=rhdm-7.4.0-decision-central-eap7-deployable.zip
+RHDM_CENTRAL_ZIP=rhdm-7.5.0-decision-central-eap7-deployable.zip
 JBOSS_EAP_ZIP=jboss-eap-7.2.0.zip
 JBOSS_EAP_PATCH_ZIP="jboss-eap-7.2.1-patch.zip jboss-eap-7.2.2-patch.zip jboss-eap-7.2.3-patch.zip jboss-eap-7.2.4-patch.zip"
 
--- rhdm-server/config.sh
+++ rhdm-server/config.sh
@@ -3,7 +3,7 @@
 DOWNLOADS_URL=https://s3-ap-southeast-2.amazonaws.com/public.juliaaano
 DOWNLOADS_DIR=./downloads
 INSTALLATION_DIR=./target
-RHDM_SERVER_ZIP=rhdm-7.4.0-kie-server-ee8.zip
+RHDM_SERVER_ZIP=rhdm-7.5.0-kie-server-ee8.zip
 JBOSS_EAP_ZIP=jboss-eap-7.2.0.zip
 JBOSS_EAP_PATCH_ZIP="jboss-eap-7.2.1-patch.zip jboss-eap-7.2.2-patch.zip jboss-eap-7.2.3-patch.zip jboss-eap-7.2.4-patch.zip"
 
